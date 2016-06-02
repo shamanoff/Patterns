6fb79632df6f7bb6bc65ec9031d925b39e05aa55
michaelb <shamanoff@gmail.com>
2016-06-02 10:01:06 +0300

    work abstract

--- AbstractFactory/src/Main.java
+++ AbstractFactory/src/Main.java
@@ -1,26 +1,39 @@
 import factories.RusFactory;
 import factories.TransFact;
 import factories.UsaFactory;
+import trans.faces.Aircraft;
+import trans.faces.Car;
 
 /**
  * Created by Michael on 06/05/2016.
  */
 public class Main {
-
-    private static TransFact fact;
+//  private static TransFact fact;
     public static void main(String[] args) {
-        if (true) {
-            fact = new RusFactory();
-        } else {
-            fact = new UsaFactory();
-        }
-        fact.createAircraft();
-        fact.createCar();
+//        if (true) {
+//            fact = new RusFactory();
+//        } else {
+//            fact = new UsaFactory();
+//        }
+//        fact.createAircraft();
+//        fact.createCar();
         //realization version
+        TransFact rusFactory = new RusFactory();
+        TransFact usaFactory = new UsaFactory();
+        Aircraft aircraft = rusFactory.createAircraft();
+        Aircraft aircraft1 = usaFactory.createAircraft();
+        Car car = rusFactory.createCar();
+        Car car1 = usaFactory.createCar();
 
-//        TransFact rusFactory = new RusFactory();
-//        Aircraft aircraft = rusFactory.createAircraft();
-//        aircraft.flight();
+        car1.drive();
+
+        car.drive();
+
+        aircraft1.flight();
+
+        aircraft.flight();
+
+
 
     
 
